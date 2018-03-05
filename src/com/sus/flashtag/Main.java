deea1f67a686ccdb6bccc6c8dbfb4e3ee20beadb
TylerZT <THMCTyler@gmail.com>
2018-03-04 17:57:58 -0800

    Start

---
+++ src/com/sus/flashtag/Main.java
@@ -0,0 +1,16 @@
+package com.sus.flashtag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{
+	
+	public void onEnable() {
+		getLogger().info("FlashTag v0.1 has been enabled!");
+	}
+	
+	public void onDisable() {
+		getLogger().info("FlashTag v0.1 has been disabled!");
+	}
+	
+
+}
